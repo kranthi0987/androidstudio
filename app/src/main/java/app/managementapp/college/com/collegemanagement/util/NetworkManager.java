b571e92c840d8d03fb830953ddf06a9309c24c4b
Sanjay <kranthi0987@gmail.com>
2016-08-24 20:11:27 +0530

    AI-2.1.3 <Sanjay@Sanjay-Dev Overwrite remote https://github.com/kranthi0987/CollegeManagement-ManagementApp to local

--- app/src/main/java/app/managementapp/college/com/collegemanagement/util/NetworkManager.java
+++
@@ -1,8 +0,0 @@
-package app.managementapp.college.com.collegemanagement.util;
-
-/**
- * Created by new on 4/1/2016.
- */
-public class NetworkManager {
-
-}
--- app/src/main/java/app/managementapp/college/com/collegemanagement/util/UIHelper.java
+++
@@ -1,9 +0,0 @@
-package app.managementapp.college.com.collegemanagement.util;
-
-/**
- * Created by new on 3/30/2016.
- */
-public class UIHelper {
-
-
-}
